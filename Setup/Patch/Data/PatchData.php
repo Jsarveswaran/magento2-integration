6a522547016f13fa846181ecab2ed0fb05b5fc77
Sarvesh <sarveswaran.j@saleswarp.com>
2024-07-01 17:04:25 +0530

    * Debug for unistall script

--- Setup/Patch/Data/PatchData.php
+++ Setup/Patch/Data/PatchData.php
@@ -5,6 +5,10 @@ namespace Saleswarp\SaleswarpShipDev\Setup\Patch\Data;
 
 use Magento\Integration\Model\ConfigBasedIntegrationManager;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
 
 /**
  * Setup data patch class to change config path
@@ -18,11 +22,26 @@ class PatchData implements DataPatchInterface
      private $integrationManager;
 
     /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
      * @param ConfigBasedIntegrationManager $integrationManager
      */
-    public function __construct(ConfigBasedIntegrationManager $integrationManager)
+    public function __construct(
+    	ConfigBasedIntegrationManager $integrationManager,
+    	ModuleDataSetupInterface $moduleDataSetup,
+    	EavSetupFactory $eavSetupFactory)
     {
         $this->integrationManager = $integrationManager;
+		$this->moduleDataSetup    = $moduleDataSetup;
+        $this->eavSetupFactory    = $eavSetupFactory;
     }
 
     /**
@@ -31,6 +50,22 @@ class PatchData implements DataPatchInterface
     public function apply()
     {
         $this->integrationManager->processIntegrationConfig(['SaleswarpShipDev']);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->delete($setup->getTable('patch_list'), ['patch_name = ?' => 'Saleswarp\SaleswarpShipDev%']);
+        $eavSetup->delete($setup->getTable('integration'), ['name = ?' => 'SaleswarpShipDev%']);
+		$eavSetup->query("DELETE FROM `patch_list` WHERE `patch_list`.`patch_name` LIKE 'Saleswarp\SaleswarpShipDev%'");
+ 
+        $this->moduleDataSetup->getConnection()->endSetup();
     }
 
     /**
